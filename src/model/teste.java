1405889c19496827a55523f7bc8c1c708a28467e
ezequiel _Specter <ezequiel1de1oliveira@gmail.com>
2017-11-02 21:55:10 -0200

    primeiro teste de grafico

--- src/model/teste.java
+++ src/model/teste.java
@@ -1,13 +1,23 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class teste {
 
 	public static void main(String[] args) {
 		
-	    PotenciaAparente teste = new PotenciaAparente();
-	   
-	     System.out.println( teste.CalcularPotenciaAparente(10,1));
+		List<Double> scores = new ArrayList<>();		
+
+        for (float i = 0; i < 10; i = (float) (i + 0.1)) {
+            scores.add(Math.cos(i));
+        }
+
+		VerGrafico view = new VerGrafico(scores);
+	}
+
 
 	}
 
-}
+
