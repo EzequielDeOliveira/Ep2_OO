56d0be2d303e650e955592dfa2786e58fd7b2703
ezequiel _Specter <ezequiel1de1oliveira@gmail.com>
2017-11-03 00:12:45 -0200

    manutençao na onda generica

--- src/model/FormaDeOnda.java
+++ src/model/FormaDeOnda.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 
 public class FormaDeOnda {
 	 
-	private Amplitude amplitude = new Amplitude();
-	private AnguloSenoide angulo = new AnguloSenoide();
+	 protected Amplitude amplitude = new Amplitude();
+	 protected AnguloSenoide angulo = new AnguloSenoide();
 	protected ArrayList<Double> pontosNoGrafico = new ArrayList<Double>();
 	private float tempoVariavel;
-	private float frequenciaAngular = (float) (2 * Math.PI * 60);
+	protected float frequenciaAngular = (float) (2 * Math.PI * 60);
 	int MaximoDepontos = 10;
 	
 	public float getTempoVariavel() {
