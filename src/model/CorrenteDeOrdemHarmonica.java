c48a9eb19a0e9f3a66919b6a2809c01149fb589d
ezequiel _Specter <ezequiel1de1oliveira@gmail.com>
2017-11-03 01:30:56 -0200

    corrente harmonica

---
+++ src/model/CorrenteDeOrdemHarmonica.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.util.ArrayList;
+
+public class CorrenteDeOrdemHarmonica extends FormaDeOnda{
+
+	private int ordemHarmonica;
+	
+	
+	CorrenteDeOrdemHarmonica() {}
+
+	public int getOrdemHarmonica() {
+		return ordemHarmonica;
+	}
+
+	public void setOrdemHarmonica(int ordemHarmonica) {
+		this.ordemHarmonica = ordemHarmonica;
+	}
+	
+	public ArrayList<Double> GeralistaOrdemHarmonica(int corrente , int anguloharmonico){
+		
+		amplitude.setAmplitudeCorrente(corrente);
+		angulo.setAnguloHarmonico(anguloharmonico);
+		
+		for(setTempoVariavel(0); getTempoVariavel() < MaximoDepontos ;   setTempoVariavel((float) (getTempoVariavel() + 0.1))) {
+		
+		pontosNoGrafico.add(amplitude.getAmplitudeCorrente() * Math.cos(getOrdemHarmonica()*frequenciaAngular * getTempoVariavel() + angulo.getAnguloHarmonico()));	
+	}
+		
+		return pontosNoGrafico;
+	}
+	
+}
