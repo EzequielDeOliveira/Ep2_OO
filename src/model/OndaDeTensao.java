d9663198deda7447f29c89e3297b8de97978a987
ezequiel _Specter <ezequiel1de1oliveira@gmail.com>
2017-11-03 00:13:39 -0200

    onde de tensao

--- src/model/OndaDeTensao.java
+++ src/model/OndaDeTensao.java
@@ -1,17 +1,27 @@
 package model;
 
+import java.util.ArrayList;
+
 public class OndaDeTensao extends FormaDeOnda{
 	
 	OndaDeTensao(){}
 	
-	public void GeraListaTensao(int tensao, int angulotensao) {
+	public ArrayList<Double> GeraListaTensao(int tensao, int angulotensao) {
+		
+		amplitude.setAmplitudeTensao(tensao);
+		angulo.setAnguloTensao(angulotensao);
 		
 		for(setTempoVariavel(0); getTempoVariavel() < MaximoDepontos ;   setTempoVariavel((float) (getTempoVariavel() + 0.1))) {
 		
-		pontosNoGrafico.add((double) getTempoVariavel());	
-			
-		}
+		pontosNoGrafico.add(amplitude.getAmplitudeTensao() * Math.cos(frequenciaAngular * getTempoVariavel() + angulo.getAnguloTensao()));	
+	}
+		return pontosNoGrafico;
 		
 	}
-
+	
+	
+	
+	
+	
+	
 }
