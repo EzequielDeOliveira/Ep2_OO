1d152557f1aee0f17af98b2d3d2f48ba00ed33e0
ezequiel _Specter <ezequiel1de1oliveira@gmail.com>
2017-11-04 18:53:54 -0200

    incrementando funcionalidade

--- src/model/DistorcidaResultaFourier.java
+++ src/model/DistorcidaResultaFourier.java
@@ -8,17 +8,18 @@ public class DistorcidaResultaFourier extends FormaDeOnda {
 	TensaoDeOrdemHarmonica tensaoHarmonica = new TensaoDeOrdemHarmonica();
 	ArrayList<Double> listaTensao = new ArrayList<Double>();
 	ArrayList<Double> listaTensaoHarmonica = new ArrayList<Double>();
-	
+	ArrayList<Double> listaTensaoHarmonicaAuxiliar = new ArrayList<Double>();
+
 	
 	DistorcidaResultaFourier(){}
 	
-	public ArrayList<Double> GeraListaDistorcidaResultanteUmHarmonico(int tensao , int angulotensao , int anguloharmonica){
+	public ArrayList<Double> GeraListaDistorcidaResultanteUmHarmonico(int tensao , int angulotensao , int anguloharmonica , int ordemharmonica){
 		amplitude.setAmplitudeTensao(tensao);
 		angulo.setAnguloTensao(angulotensao);
 		angulo.setAnguloHarmonico(anguloharmonica);
 		
 		listaTensao = ondaDaTensao.GeraListaTensao(amplitude.getAmplitudeTensao(),angulo.getAnguloTensao());
-		listaTensaoHarmonica = tensaoHarmonica.GeralistaOrdemHarmonica(amplitude.getAmplitudeTensao(), angulo.getAnguloHarmonico());
+		listaTensaoHarmonica = tensaoHarmonica.GeralistaOrdemHarmonica(amplitude.getAmplitudeTensao(), angulo.getAnguloHarmonico() ,ordemharmonica);
 		
 		for(setTempoVariavel(0); getTempoVariavel() < MaximoDepontos ;setTempoVariavel(getTempoVariavel() + 1)) {
 			
@@ -30,7 +31,23 @@ public class DistorcidaResultaFourier extends FormaDeOnda {
 	}
 	
 	
-	
+	public ArrayList<Double> GeraListaDistorcidaResultanteDoisHarmonicos(int tensao , int tensao2 ,int angulotensao ,int angulotensao2, int anguloharmonico ,int anguloharmonica2 , int ordemharmonica , int ordemharmonica2){
+		amplitude.setAmplitudeTensao(tensao2);
+		angulo.setAnguloTensao(angulotensao2);
+		angulo.setAnguloHarmonico(anguloharmonica2);
+		
+		listaTensaoHarmonicaAuxiliar = GeraListaDistorcidaResultanteUmHarmonico(tensao,angulotensao,anguloharmonico,ordemharmonica);
+		//inacabado
+		listaTensao = onda
+		for(setTempoVariavel(0); getTempoVariavel() < MaximoDepontos ;setTempoVariavel(getTempoVariavel() + 1)) {
+			
+			pontosNoGrafico.add(listaTensao.get((int) getTempoVariavel()) + listaTensaoHarmonica.get((int) getTempoVariavel()));
+				
+		}
+		
+		return pontosNoGrafico;
+	}
+
 	
 
 }
