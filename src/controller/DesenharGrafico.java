80fd3325846b67ab880c01f1ffb2f1c5e7460ce5
ezequiel _Specter <ezequiel1de1oliveira@gmail.com>
2017-11-12 21:37:20 -0200

    atualizaçao do grafico

--- src/controller/DesenharGrafico.java
+++ src/controller/DesenharGrafico.java
@@ -15,8 +15,10 @@ import javax.swing.JPanel;
 
 public class DesenharGrafico extends JPanel {
 	
-	private int padding = 25;
-    private int labelPadding = 25;
+	private int padding = 5;
+    private int labelPadding = 5;
+    private int width = 400;
+    private int height = 150;
     private Color lineColor;
 	private Color pointColor = new Color(100, 100, 100, 180);
     private Color gridColor = new Color(200, 200, 200, 200);
@@ -25,13 +27,29 @@ public class DesenharGrafico extends JPanel {
     private int numberYDivisions = 10;
     private List<Double> scores;
 
-    public DesenharGrafico(List<Double> scores) {
+    public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public DesenharGrafico(List<Double> scores) {
         this.scores = scores;
+    }
+    
+    public int getWidth() {
+    		return this.width;
+    }
+    
+    public int getHeight() {
+    		return this.height;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+    	super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
@@ -45,53 +63,44 @@ public class DesenharGrafico extends JPanel {
             graphPoints.add(new Point(x1, y1));
         }
 
-        // draw white background
+        // Desenha o fundo do gráfico
         g2.setColor(Color.WHITE);
         g2.fillRect(padding + labelPadding, padding, getWidth() - (2 * padding) - labelPadding, getHeight() - 2 * padding - labelPadding);
         g2.setColor(Color.BLACK);
 
-        // create hatch marks and grid lines for y axis.
+        // Desenha as Linhas para X e Y
+        
+        // Linha para Y
         for (int i = 0; i < numberYDivisions + 1; i++) {
             int x0 = padding + labelPadding;
             int x1 = pointWidth + padding + labelPadding;
             int y0 = getHeight() - ((i * (getHeight() - padding * 2 - labelPadding)) / numberYDivisions + padding + labelPadding);
             int y1 = y0;
-            if (scores.size() > 0) {
-                g2.setColor(gridColor);
-                g2.drawLine(padding + labelPadding + 1 + pointWidth, y0, getWidth() - padding, y1);
-                g2.setColor(Color.BLACK);
-                String yLabel = ((int) ((getMinScore() + (getMaxScore() - getMinScore()) * ((i * 1.0) / numberYDivisions)) * 100)) / 100.0 + "";
-                FontMetrics metrics = g2.getFontMetrics();
-                int labelWidth = metrics.stringWidth(yLabel);
-                g2.drawString(yLabel, x0 - labelWidth - 5, y0 + (metrics.getHeight() / 2) - 3);
-            }
-            g2.drawLine(x0, y0, x1, y1);
+            g2.setColor(Color.BLACK);
+            if(i%2==0)
+                g2.drawLine(x0, y0, x1, y1);
         }
-
-        // and for x axis
+        
+        // Linha para X
         for (int i = 0; i < scores.size(); i++) {
             if (scores.size() > 1) {
                 int x0 = i * (getWidth() - padding * 2 - labelPadding) / (scores.size() - 1) + padding + labelPadding;
                 int x1 = x0;
                 int y0 = getHeight() - padding - labelPadding;
                 int y1 = y0 - pointWidth;
-                if ((i % ((int) ((scores.size() / 20.0)) + 1)) == 0) {
-                    g2.setColor(gridColor);
-                    g2.drawLine(x0, getHeight() - padding - labelPadding - 1 - pointWidth, x1, padding);
-                    g2.setColor(Color.BLACK);
-                    String xLabel = i + "";
-                    FontMetrics metrics = g2.getFontMetrics();
-                    int labelWidth = metrics.stringWidth(xLabel);
-                    g2.drawString(xLabel, x0 - labelWidth / 2, y0 + metrics.getHeight() + 3);
-                }
-                g2.drawLine(x0, y0, x1, y1);
+                g2.setColor(Color.BLACK);
+                if(i%2==0)
+                    g2.drawLine(x0, y0, x1, y1);
             }
         }
-
-        // create x and y axes 
-        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, padding + labelPadding, padding); // y axi
-        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, getWidth() - padding, getHeight() - padding - labelPadding); // x axi
-
+        
+        // Cria a moldura para o gráfico
+        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, padding + labelPadding, padding);
+        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, getWidth() - padding, getHeight() - padding - labelPadding);
+        g2.drawLine(getWidth() - padding, getHeight() - padding - labelPadding, getWidth() - padding, padding);
+        g2.drawLine(padding + labelPadding, padding, getWidth() - padding, padding);
+        
+        // Desenha as linhas entre os pontos do gráfico
         Stroke oldStroke = g2.getStroke();
         g2.setColor(lineColor);
         g2.setStroke(GRAPH_STROKE);
