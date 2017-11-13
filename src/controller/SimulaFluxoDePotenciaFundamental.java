3e6cdad62d83e6748df0575a57f7f32077f1a180
ezequiel _Specter <ezequiel1de1oliveira@gmail.com>
2017-11-13 01:03:11 -0200

    iniciando controller

--- src/controller/SimulaFluxoDePotenciaFundamental.java
+++ src/controller/SimulaFluxoDePotenciaFundamental.java
@@ -1,16 +1,22 @@
-//package controller;
-//
-//public class SimulaFluxoDePotenciaFundamental extends Simulacao {
-//
-//	
-//	
-//	//public ArrayList <Double> GeraListaOndaFundamental(int tensao , int anguloTensao) {
-//		
-//		
-//		
-//		
-//		
-//	}
-//
-//	
-//}
+package controller;
+
+import java.util.ArrayList;
+
+import model.OndaDeTensao;
+
+public class SimulaFluxoDePotenciaFundamental extends Simulacao {
+
+	
+	
+	public ArrayList<Double> GeraListaOndaFundamental(int tensao , int anguloTensao) {
+		
+		OndaDeTensao ondaTensao = new OndaDeTensao();
+		
+		listaResultado = ondaTensao.GeraListaTensao(tensao, anguloTensao);
+		
+		return listaResultado;	
+		
+	}
+
+	
+}
